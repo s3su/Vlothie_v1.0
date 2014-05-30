9b3609c1982e0c96aa148c4df48940bf550256ed
juandalvit <juandalvit>
2014-05-30 14:07:08 -0300

    Correct Look

--- Resources/alloy/CFG.js
+++ Resources/alloy/CFG.js
@@ -1,1 +1,1 @@
-module.exports={"dependencies":{}};
+module.exports={"dependencies":{"com.appcelerator.loading":"1.0"}};
--- Resources/iphone/alloy/controllers/menu.js
+++ Resources/iphone/alloy/controllers/menu.js
@@ -2,33 +2,6 @@ function Controller() {
     function showWindows(e) {
         0 == e.index ? Alloy.createController("look").getView().open() : 1 == e.index ? Alloy.createController("trend").getView().open() : 2 == e.index ? Alloy.createController("alarm").getView().open() : 3 == e.index ? Alloy.createController("settings").getView().open() : 4 == e.index && Alloy.createController("home").getView().open();
     }
-    function handleMenuClick(_event) {
-        "undefined" != typeof _event.row.id && openScreen(_event.row.id);
-    }
-    function openMenu() {
-        $.AppWrapper.animate({
-            left: "200dp",
-            duration: 250,
-            curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-        $.SlideMenu.Wrapper.animate({
-            left: "0dp",
-            duration: 250,
-            curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-    }
-    function closeMenu() {
-        $.AppWrapper.animate({
-            left: "0dp",
-            duration: 250,
-            curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-        $.SlideMenu.Wrapper.animate({
-            left: "-200dp",
-            duration: 250,
-            curve: Ti.UI.ANIMATION_CURVE_EASE_IN_OUT
-        });
-    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "menu";
     var __parentSymbol = arguments[0] ? arguments[0]["__parentSymbol"] : null;
@@ -73,43 +46,15 @@ function Controller() {
     });
     $.__views.footer.add($.__views.tabbedbar);
     showWindows ? $.__views.tabbedbar.addEventListener("click", showWindows) : __defers["$.__views.tabbedbar!click!showWindows"] = true;
-    $.__views.SlideMenu = Alloy.createWidget("com.mcongrove.slideMenu", "widget", {
-        id: "SlideMenu",
+    $.__views.loading = Alloy.createWidget("com.appcelerator.loading", "widget", {
+        id: "loading",
         __parentSymbol: __parentSymbol
     });
-    $.__views.SlideMenu && $.addTopLevelView($.__views.SlideMenu);
-    $.__views.AppWrapper = Ti.UI.createView({
-        id: "AppWrapper"
-    });
-    $.__views.AppWrapper && $.addTopLevelView($.__views.AppWrapper);
+    $.__views.loading && $.addTopLevelView($.__views.loading);
     exports.destroy = function() {};
     _.extend($, $.__views);
-    var nodes = [ {
-        menuHeader: "My Tabs",
-        id: 0,
-        title: "Home",
-        image: "/images/home.png"
-    }, {
-        id: 1,
-        title: "Contact",
-        image: "/images/phone.png"
-    }, {
-        id: 2,
-        title: "Settings",
-        image: "/images/gear.png"
-    } ];
-    $.SlideMenu.init({
-        nodes: nodes,
-        color: {
-            headingBackground: "#000",
-            headingText: "#FFF"
-        }
-    });
-    $.SlideMenu.setIndex(0);
-    $.SlideMenu.Nodes.addEventListener("click", handleMenuClick);
-    $.AppWrapper.addEventListener("swipe", function(_event) {
-        "right" == _event.direction ? openMenu() : "left" == _event.direction && closeMenu();
-    });
+    $.loading.setOpacity(1);
+    $.loading.setOpacity(0);
     __defers["$.__views.tabbedbar!click!showWindows"] && $.__views.tabbedbar.addEventListener("click", showWindows);
     _.extend($, exports);
 }
